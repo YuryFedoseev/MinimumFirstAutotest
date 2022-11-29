4abeb94ce0a3ec70d77324556f31195a7d33375a
Yury.Fedoseev <Yri95@inbox.ru>
2022-11-29 23:03:34 +0300

    second crach test

---
+++ src/test/java/test/Crach.java
@@ -0,0 +1,31 @@
+package test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class Crach {
+    @Test
+    void crashSearchTest() {
+        Configuration.holdBrowserOpen = true;
+        open("https://demoqa.com/automation-practice-form");
+        // $(byId("firstName")).setValue("Dmitri").pressEnter();
+        $("#firstName").setValue("Dmitro").pressEnter();
+        $(byId("lastName")).setValue("Ivanov").pressEnter();
+        $(byId("userEmail")).setValue("ivanov@mail.com").pressEnter();
+        $(byTagAndText("Label", "Male")).click();
+        $(byId("userNumber")).setValue("9012223344").pressEnter();
+
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("Muha"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("ivanov@mail.com"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("Male"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("9012223344"));
+
+    }
+
+}
--- src/test/java/test/FirstForm.java
+++ src/test/java/test/FirstForm.java
@@ -1,7 +1,11 @@
+package test;
+
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.*;
@@ -11,12 +15,20 @@ public class FirstForm {
     void successfulSearchTest() {
         Configuration.holdBrowserOpen = true;
         open("https://demoqa.com/automation-practice-form");
-        $(byId("firstName")).setValue("Dmitri").pressEnter();
+        // $(byId("firstName")).setValue("Dmitri").pressEnter();
+        $("#firstName").setValue("Dmitro").pressEnter();
         $(byId("lastName")).setValue("Ivanov").pressEnter();
         $(byId("userEmail")).setValue("ivanov@mail.com").pressEnter();
-        $(byTagAndText("Label","Male")).click();
+        $(byTagAndText("Label", "Male")).click();
         $(byId("userNumber")).setValue("9012223344").pressEnter();
 
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("Dmitro"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("ivanov@mail.com"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("Male"));
+        $x("//div[@class='table-responsive']").shouldHave(Condition.text("9012223344"));
+        //$("tr").find("Student Name").findAll("curlik");
+        //$$("tr").shouldHave(text("curlik"));
+        //$("#tr").find("Student Name").shouldHave(text("Student Name"));
         //$("#dateOfBirthInput").setValue("29.11.2021");
         //$$("class").findBy(Condition.attribute("react-datepicker-wrapper")).setValue("02.07.2000").pressEnter();
         //$$(byXpath("@class","react-datepicker__input-container")).setValue("02.07.2000").pressEnter();
